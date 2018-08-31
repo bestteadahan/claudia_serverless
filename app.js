a43662aaa609089b75fde188d372296cfaeca049
Tea Lin <tea.lin@wattertek.com>
2018-08-31 10:35:56 +0800

    final version

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const parser = require('body-parser')
 const app = express()
 
+app.use(parser.json())
+// app.use(parser.urlencoded())
+
 app.get('/', (req, res) => {
-    res.send(`Hello, ${req.query.name}`)
+    res.send(`Hello, ${req.query.name}. This response comes from GET.`)
+})
+
+app.post('/', (req, res) => {
+    res.send(`Hello, ${req.body.name}. This response comes from POST.`)
 })
 
 module.exports = app
